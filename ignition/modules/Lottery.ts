ba2519699761f71dc7ed57bdc3ea6bdb53bc1659
hfccr <hfccr@outlook.com>
2024-07-17 22:59:54 +0530

    Add lottery deployment module

---
+++ ignition/modules/Lottery.ts
@@ -0,0 +1,9 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const LotteryModule = buildModule("LotteryModule", (m) => {
+  const lottery = m.contract("Lottery", [], {});
+
+  return { lottery };
+});
+
+export default LotteryModule;
